173e8d2a7dc2a8ce57a039c09cd86a22eff37a67
Victor Ayuste <ayuste2410@gmail.com>
2023-11-02 23:23:51 +0100

    cambios index

--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ const cors = require('cors')
 const port = process.env.PORT || 3000
 const dbURI = 'mongodb+srv://ayuste:rootayuste@ayustegg.pee5oql.mongodb.net/?retryWrites=true&w=majority'
 
-app.use(cors())
+const corsOptions = {
+  origin: 'http://35.181.53.241'
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
-app.use(cors())
+
 // routesapp.use(cors());
 app.use('/api/v1/toDo', v1TodoRouter)
 
